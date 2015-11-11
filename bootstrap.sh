63211ecef16e01022d2751dc21ca3d61c3205890
Shawn Meginley <smjest0802@gmail.com>
2015-11-11 16:47:31 -0500

    Got MySQL Server install working and password for root set properly.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,20 +6,21 @@ apt-get update
 default_password="changeme"
 
 # Install PIP
-sudo apt-get install -y python-pip
+#sudo apt-get install -y python-pip
 
 # Install mySQL Server
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${default_password}'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${default_password}'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${default_password}'
+echo mysql-server mysql-server/root_password password ${default_password} | sudo debconf-set-selections
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${default_password}'
+echo mysql-server mysql-server/root_password_again password ${default_password} | sudo debconf-set-selections
 sudo apt-get install -y mysql-server
+#mysql_secure_installation
 
+# Python Configuration
 #easy_install django
 #sudo apt-get install libmysqlclient-dev
 #sudo apt-get install python-dev
 #pip install mysql-python
 
-# MySQL specific
-#mysql_secure_installation
-
 #Configure
-#adduser family
+#useradd -m -p $default_password -s /bin/bash family
